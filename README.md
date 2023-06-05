ad520196ca4917be40e2af41e40703bbe1fcaf1c
Steven <52871164+sstonaker@users.noreply.github.com>
2023-06-04 20:11:29 -0500

    update in prep for chrome store

---
+++ README.md
@@ -0,0 +1,21 @@
+# Youtube Playback Times
+
+Youtube Playback Times is a Google Chrome extension that prepends the video's duration to
+your Youtube bookmarks.
+
+If you ever find yourself bookmarking lots of videos to watch later, this extension is for you!
+
+You can manage your available time better by selecting videos from your bookmarks that correspond
+to the time you have available for viewing.
+
+## Installation and Use
+
+You will need a Google API key to use this extension. Unfortunately video durations are behind Google's API.
+
+[See this link for an overview of the Google API](https://developers.google.com/youtube/v3/getting-started)
+
+[Instructions for creating an API key can be found here](https://developers.google.com/youtube/registering_an_application)
+
+### Chrome Extension Store
+
+Pending Approval
--- popup.js
+++ popup.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const runButton = document.querySelector('#target');
-    // onClick's logic below:
-    runButton.addEventListener('click', (event) => {
-        chrome.runtime.sendMessage(document.querySelector('#api-key').value);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const runButton = document.querySelector("#target");
+  // onClick's logic below:
+  runButton.addEventListener("click", (event) => {
+    chrome.runtime.sendMessage(document.querySelector("#api-key").value);
+  });
 
+  chrome.runtime.onMessage.addListener(function (message) {
+    displayMessage(message);
+  });
+
+  function displayMessage(message) {
+    let messageArea = document.querySelector("#message");
+    messageArea.innerHTML = message;
+  }
 });
